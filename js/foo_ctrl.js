9f50a2658bd0ebab74282928b48003794b19ba90
Zach <briggszj@gmail.com>
2014-08-10 11:59:31 -0500

    Add sum example.

--- js/foo_ctrl.js
+++ js/foo_ctrl.js
@@ -2,11 +2,20 @@
   "use strict";
 
   angular.module("purpleHippo").controller("fooCtrl", function ($scope) {
-    $scope.dog = "Max";
+    $scope.firstNumber = "0";
+    $scope.secondNumber = "0";
 
 
-    $scope.dogName = function() {
-      return $scope.dog;
+    $scope.sum = function() {
+      var first = parseInt($scope.firstNumber);
+      var second = parseInt($scope.secondNumber);
+      var result = first + second;
+
+      if (result + 0 === result) {
+        return result;
+      } else {
+        return "Please enter a valid number. Like, for reals.";
+      }
     };
   });
 
